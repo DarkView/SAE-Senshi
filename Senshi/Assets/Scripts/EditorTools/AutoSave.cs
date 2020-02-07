fb0e182cb8f5463585e00213e4873efd93a184ed
Dark <7890309+DarkView@users.noreply.github.com>
2020-02-07 20:17:56 +0100

    Add basic keybind functionality

---
+++ Senshi/Assets/Scripts/EditorTools/AutoSave.cs
@@ -0,0 +1,38 @@
+﻿#if UNITY_EDITOR 
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using Debug = UnityEngine.Debug;
+
+// Based on https://www.reddit.com/r/Unity3D/comments/97bt65/autosave_on_run_editor_script/
+// Added by Nils
+
+/// <summary>
+/// Class used to autosave the open scenes once we click the play button 
+/// </summary>
+[InitializeOnLoad]
+public class AutoSave
+{
+    /// <summary>
+    /// Called by InitializeOnLoad, adds the SaveOnPlay method to the list of methods to execute when we change play mode
+    /// </summary>
+    static AutoSave()
+    {
+        EditorApplication.playModeStateChanged += SaveOnPlay;
+    }
+
+    /// <summary>
+    /// Called every time we change the play mode, only saves if the mode is exiting edit mode
+    /// </summary>
+    /// <param name="state"></param>
+    private static void SaveOnPlay(PlayModeStateChange state)
+    {
+        if (state == PlayModeStateChange.ExitingEditMode)
+        {
+            Debug.Log("Entering Play Mode - Saving!");
+            EditorSceneManager.SaveOpenScenes();
+            AssetDatabase.SaveAssets();
+        }
+    }
+
+}
+#endif
---
+++ Senshi/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
+
+/// <summary>
+/// Basic inputhandling and custom keybinds
+/// By Nils
+/// </summary>
+public class InputHandler : MonoBehaviour
+{
+
+    private string cfgFolderBase = "./cfg/";
+    private Dictionary<InputOption, KeyCode> keybinds = new Dictionary<InputOption, KeyCode>();
+
+    /// <summary>
+    /// Load or create all keybinds
+    /// </summary>
+    void Awake()
+    {
+
+        if (!Directory.Exists(cfgFolderBase))
+        {
+            Directory.CreateDirectory(cfgFolderBase);
+        }
+
+        if (File.Exists(cfgFolderBase + "keys.json"))
+        {
+            keybinds = (Dictionary<InputOption, KeyCode>) JsonConvert.DeserializeObject(cfgFolderBase + "keys.json");
+        }
+        else
+        {
+            // Default Keybinds
+            // Player 1
+            keybinds.Add(InputOption.P1_Left, KeyCode.A);
+            keybinds.Add(InputOption.P1_Right, KeyCode.D);
+            keybinds.Add(InputOption.P1_Up, KeyCode.W);
+            keybinds.Add(InputOption.P1_Down, KeyCode.S);
+
+            keybinds.Add(InputOption.P1_LPunch, KeyCode.R);
+            keybinds.Add(InputOption.P1_RPunch, KeyCode.T);
+
+            // Player 2  InputOption.  
+            keybinds.Add(InputOption.P2_Left, KeyCode.J);
+            keybinds.Add(InputOption.P2_Right, KeyCode.L);
+            keybinds.Add(InputOption.P2_Up, KeyCode.I);
+            keybinds.Add(InputOption.P2_Down, KeyCode.K);
+
+            keybinds.Add(InputOption.P2_LPunch, KeyCode.Keypad4);
+            keybinds.Add(InputOption.P2_RPunch, KeyCode.Keypad5);
+            
+            StreamWriter sw = File.CreateText(cfgFolderBase + "keys.json");
+            sw.Write(JsonConvert.SerializeObject(keybinds));
+            sw.Close();
+        }
+        
+    }
+
+    public bool ChangeKeybind(InputOption bind, KeyCode key)
+    {
+        foreach (KeyCode kc in keybinds.Values)
+        {
+            if (kc == key)
+            {
+                throw new ArgumentException("That key is already bound!");
+            }
+        }
+
+        keybinds[bind] = key;
+        return true;
+    }
+
+    /*
+    void Update()
+    {
+        
+    }
+    */
+
+    public enum InputOption
+    {
+        P1_Left = 0,
+        P1_Right,
+        P1_Up,
+        P1_Down,
+        P1_LPunch,
+        P1_RPunch,
+        P2_Left,
+        P2_Right,
+        P2_Up,
+        P2_Down,
+        P2_LPunch,
+        P2_RPunch,
+    }
+
+}
