d0ee0e986fbd4d289af9423623f89012f4a26a4b
Zayarmoe <z.kyaw24@gmail.com>
2020-02-10 11:21:11 +0100

    Main Menu finished; Settings in progress;

---
+++ Senshi/Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Main Menu Buttons and input loops
+/// by Zayarmoe
+/// </summary>
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Object startScene;
+    [SerializeField] private Object settingsScene;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(startScene.name, LoadSceneMode.Single);
+    }
+
+    public void OpenSettings()
+    {
+        SceneManager.LoadScene(settingsScene.name, LoadSceneMode.Additive);
+    }
+
+    public void closeApplication()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+
+    }
+}
---
+++ Senshi/Assets/Scripts/Menu/Settings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/// <summary>
+/// SettingsButtons
+/// by Zayarmoe
+/// </summary>
+public class Settings : MonoBehaviour
+{
+    [SerializeField] private Slider audio;
+
+    private void Update()
+    {
+        ChangeAudio();
+    }
+
+    private void ChangeAudio()
+    {
+        AudioListener.volume = audio.value;
+    }
+
+    public void CloseScene()
+    {
+        SceneManager.UnloadSceneAsync(this.gameObject.scene);
+    }
+}
