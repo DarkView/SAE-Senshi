4bd8de286cd4792ba90fc5d4494e2ed440231984
Louis Schmidberger <louisschmiddi@gmx.de>
2020-02-29 19:00:40 +0100

    Story Manager and CutSceneManager Prototype

---
+++ Senshi/Assets/Scripts/StoryMode/CutSceneManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
+
+public class CutSceneManager : MonoBehaviour
+{
+    [SerializeField] private GameObject panel;
+    [SerializeField] private Sprite openingScene;
+    [SerializeField] private Sprite riu;
+    [SerializeField] private Sprite akai;
+    [SerializeField] private Sprite akumaPre;
+    [SerializeField] private Sprite akumaMid;
+    [SerializeField] private Sprite endScene;
+
+
+    public void InitCutScene(int index)
+    {
+        SceneManager.LoadScene(1);
+        switch (index)
+        {
+            case 1:
+                panel.GetComponent<Image>().sprite = openingScene;
+                break;
+            case 2:
+            case 4:
+                panel.GetComponent<Image>().sprite = riu;
+                break;
+            case 5:
+            case 7:
+                panel.GetComponent<Image>().sprite = akai;
+                break;
+            case 8:
+                panel.GetComponent<Image>().sprite = akumaPre;
+                break;
+            case 10:
+                panel.GetComponent<Image>().sprite = akumaMid;
+                break;
+            case 11:
+                panel.GetComponent<Image>().sprite = endScene;
+                break;
+                
+        }
+    }
+}
---
+++ Senshi/Assets/Scripts/StoryMode/StoryManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+public static class StoryManager
+{
+
+    public static int StoryIndex = 1;
+    public static CutSceneManager SceneManager = new CutSceneManager();
+
+    
+
+    /// <summary>
+    /// Choose Story Stage with index StoryIndex
+    /// </summary>
+    public static void LoadStage()
+    {
+        //1 --> Opening Scene
+        //2 --> Prefight Scene Riu
+        //3 --> Fight Scene Riu
+        //4 --> Postfight Scene Riu
+        //5 --> Prefight Scene Akai
+        //6 --> Fight Scene Akai
+        //7 --> Postfight Scene Akai
+        //8 --> Prefight Scene Akuma
+        //9 --> Fight Scene 1 Akuma
+        //10 --> Midfight Scene Akuma
+        //11 --> Fight Scene 2 Akuma
+        //12 --> End Scene
+
+        switch (StoryIndex)
+        {
+            case 1:
+            case 2:
+            case 4:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 11:
+                LoadCutScene(StoryIndex);
+                break;
+            case 3:
+            case 6:
+            case 9:
+                InitStage(StoryIndex);
+                break;
+        }
+
+    }
+
+    public static void LoadCutScene(int index)
+    {
+        //ruft CutSceneManager Script auf
+        SceneManager.InitCutScene(index);
+
+        //if cutscene finished
+        StoryIndex += 1;
+        StoryManager.LoadStage();
+    }
+
+    public static void InitStage(int index)
+    {
+        //ruft fight Script auf, übergibt 2 Spieler (GameObjects)
+    }
+
+    public static void StoryContinue()
+    {
+        StoryIndex += 1;
+        StoryManager.LoadStage();
+    }
+
+}
